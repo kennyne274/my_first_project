ea242d5d6d16790a09ccf42c6d59685ac9f8661a
Ray <cilver2965@naver.com>
2026-02-05 22:43:07 +0900

    Refactor comments and enhance word cloud settings
    Updated comments and adjusted word cloud parameters.

--- newstitle.py
+++ newstitle.py
@@ -1,4 +1,5 @@
 # 네이버 IT/과학 뉴스 제목 10개를 추출하여 워드 클리우드로 시각화하는 코드
+# pip install requests beautifulsoup4 wordcloud matplotlib <= 코드를 실행 하기전에 설차
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 
 # 1. 네이버 IT/과학 뉴스 URL
 url = "https://news.naver.com/section/105"
+
 
 # 2. 페이지 요청
 response = requests.get(url, headers={
@@ -29,25 +31,26 @@ else:
     print("뉴스 페이지 가져오기 실패")
 
 
-
-# 3. 제목들을 하나의 문자열로 합치기
+    # 3. 제목들을 하나의 문자열로 합치기
 text = " ".join(titles)
 
-print("워드클라우드 원본 텍스트:")
+print("워드클라우드 원본 텍스트")
 print(text)
-
 # 4. 워드클라우드 생성 (기본 버전, 마스크 없음)
 wordcloud = WordCloud(
     font_path="malgun.ttf",   # 한글 폰트 (Windows)
     width=800,
-    height=400,
-    max_font_size=120,
-    colormap="winter",
+    height=500,
+    max_words=100,  # 최대 단어 수
+    relative_scaling=0.5,
+    max_font_size=110,
+    colormap="spring",
     background_color="white"
 ).generate(text)
 
 # 5. 시각화
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(10, 7))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
+plt.savefig("wordcloud.png", dpi=300)
 plt.show()
