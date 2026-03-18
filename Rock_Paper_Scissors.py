d5e129a22b4a1dcd3f85d3b20a34bb5d3ea0d16b
Ray <cilver2965@naver.com>
2026-03-19 00:50:07 +0900

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -23,7 +23,7 @@ while True:
             break
     
         if user not in choices:
-            raise ValueError # 범위 밖의 정수를 입력하면 ValueError를 발생시킴
+            raise ValueError # 가위, 바위, 보가 아니면 ValueError를 발생시킴
 
     except ValueError:
         print("가위, 바위, 보 중에 하나만 입력하세요.")
@@ -31,35 +31,34 @@ while True:
         continue
     
 
-    else:
-        computer = random.choice(choices)
+    computer = random.choice(choices)
 
-        time.sleep(1)
-        print(f"당신의 선택 {user}")
-        time.sleep(1)
-        print(f"컴퓨터의 선택 {computer}")
-        time.sleep(1)
-        if user == computer:
-            print("비겼습니다 🤔 한판 더?")
-            draws += 1
-        elif(user == "가위" and computer == "보") or \
-            (user == "바위" and computer == "가위") or\
-            (user == "보" and computer == "바위"):
-            print("당신이 이겼습니다 😓")
-            wins += 1
-        else:
-            print("컴퓨터의 승리 😂 약 오르지?")
-            losses += 1
+    time.sleep(1)
+    print(f"당신의 선택 {user}")
+    time.sleep(1)
+    print(f"컴퓨터의 선택 {computer}")
+    time.sleep(1)
+    if user == computer:
+        print("비겼습니다 🤔 한판 더?")
+        draws += 1
+    elif(user == "가위" and computer == "보") or \
+        (user == "바위" and computer == "가위") or\
+        (user == "보" and computer == "바위"):
+        print("당신이 이겼습니다 😓")
+        wins += 1
+    else:
+        print("컴퓨터의 승리 😂 약 오르지?")
+        losses += 1
 
 # 통계 출력 및 승률 계산 
 total = wins + draws + losses
 if total > 0:
-    wins_rate = wins/total * 100
+    wins_rate = wins/total * 100  # 승률 계산
 else:
     wins_rate = 0
 
 print("="*30)
 print(f"승: {wins} | 패: {losses} | 무: {draws}")
-print(f"승률 {wins_rate:.2f}")
+print(f"승률 {wins_rate:.2f}%")
 print("="*30)
 
